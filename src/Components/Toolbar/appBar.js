44d526048ad265f9105358e71725e19ed6f071a1
Rudra665 <rudrapratapsinghbhadouria@gmail.com>
2022-11-06 12:23:46 +0530

    modified:   src/Components/Toolbar/appBar.js

--- src/Components/Toolbar/appBar.js
+++ src/Components/Toolbar/appBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Logo from "../Image/logo/logo.js";
+import { styled, useTheme } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -9,8 +10,17 @@ import Menu from "@mui/material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
-import { AppBar } from "@mui/material";
+import {
+  AppBar,
+  CssBaseline,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  SwipeableDrawer,
+} from "@mui/material";
 import { HashLink as Link } from "react-router-hash-link";
+
 const CustomAppBar = (props) => {
   const pages = [
     { id: "1", name: "Home", to: "#home" },
@@ -19,19 +29,20 @@ const CustomAppBar = (props) => {
     { id: "1", name: "Shop", to: "#Shop" },
     { id: "1", name: "Contact Us", to: "#Contact" },
   ];
-  
+
   const [anchorElNav, setAnchorElNav] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null); 
+    setAnchorElNav(null);
+    
   };
 
   return (
     <BrowserRouter>
-      <AppBar position="sticky" sx={{ background: "white", boxShadow: "none" }}>
+      <AppBar position="fixed" sx={{ background: "white", boxShadow: "none" }}>
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <Box sx={{ display: { xs: "none", md: "flex" } }}>
@@ -46,6 +57,7 @@ const CustomAppBar = (props) => {
                 display: { xs: "flex", md: "none", justifyContent: "right" },
               }}
             >
+              <CssBaseline />
               <IconButton
                 size="large"
                 aria-label="account of current user"
@@ -61,29 +73,25 @@ const CustomAppBar = (props) => {
                   style={{ width: "20px", height: "20px" }}
                 />
               </IconButton>
-
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElNav)}
+              <SwipeableDrawer
+                elevation={16}
                 onClose={handleCloseNavMenu}
                 sx={{
-                  display: { xs: "block", md: "none" },
+                  flexShrink: 0,
+                  "& .MuiDrawer-paper": {
+                    width: "100%",
+                    height: "35%",
+                    backgroundColor: "Black",
+                    align: "center",
+                  },
                 }}
+                anchor="Top"
+                open={anchorElNav}
               >
                 {pages.map((page) => (
                   <Link
                     to={page.to}
-                    style={{ textDecoration: "none"}}
+                    style={{ textDecoration: "none", color: "grey" }}
                   >
                     <MenuItem
                       key={page.id}
